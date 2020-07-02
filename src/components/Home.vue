48d18063de9c313eaffb5bf9dd9e7f8d9aff6fad
Kunju Perath <kperath@gmail.com>
2020-07-02 12:55:11 -0400

    iterating through data works. now to utilize api

--- src/components/Home.vue
+++ src/components/Home.vue
@@ -1,11 +1,11 @@
 <template>
   <v-content>
     <v-row dense>
-      <v-col cols="12">
+      <v-col v-for="(hero, i) in heroes" :key="i" cols="12">
         <v-card dark shaped class="mx-4">
-          <v-card-title class="headline">{{test}}</v-card-title>
+          <v-card-title class="headline">{{hero.name}}</v-card-title>
 
-          <v-card-subtitle>Listen to your favorite artists and albums whenever and wherever, online and offline.</v-card-subtitle>
+          <v-card-subtitle>{{hero.info}}</v-card-subtitle>
 
           <v-card-actions>
             <v-btn text>Edit</v-btn>
@@ -20,7 +20,22 @@
 export default {
   name: "Home",
   data: () => ({
-    test: 77
+    heroes: [
+      {
+        id: 1,
+        name: "Batman",
+        year: 1939,
+        info:
+          "Batman is a fictional superhero appearing in American comic books published by DC Comics. The character was created by artist Bob Kane and writer Bill Finger, and first appeared in Detective Comics #27 in 1939. Originally named the 'Bat-Man,' the character is also referred to by such epithets as the Caped Crusader, the Dark Knight, and the World's Greatest Detective. (wikipedia)"
+      },
+      {
+        id: 2,
+        name: "Owlman",
+        year: 2000,
+        info:
+          "Owlman is a fictional superhero appearing in American comic books published by DC Comics. The character was created by artist Bob Kane and writer Bill Finger (was he???), and first appeared in Detective Comics #27 in 1939. Originally named the 'Bat-Man,' the character is also referred to by such epithets as the Caped Crusader, the Dark Knight, and the World's Greatest Detective. (wikipedia)"
+      }
+    ]
   })
 };
 </script>
