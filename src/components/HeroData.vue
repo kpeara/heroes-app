0fb1b0d297f0a2b25fd491eac2e2d71911567a1e
Kunju Perath <kperath@gmail.com>
2020-07-03 01:35:35 -0400

    fixed validation. onto edit button (using put requests)

--- src/components/HeroData.vue
+++ src/components/HeroData.vue
@@ -8,7 +8,7 @@
         <span class="headline white--text">Add Hero</span>
       </v-card-title>
       <v-card-text>
-        <v-container>
+        <v-form ref="form">
           <v-row>
             <v-col cols="12" sm="6" md="4">
               <v-text-field
@@ -27,13 +27,13 @@
               <v-text-field label="Info*" required :rules="infoRules" v-model="info"></v-text-field>
             </v-col>
           </v-row>
-        </v-container>
+        </v-form>
         <small>*indicates required field</small>
       </v-card-text>
       <v-card-actions>
         <v-spacer></v-spacer>
         <v-btn color="blue darken-1" text @click="dialog = false">Close</v-btn>
-        <v-btn color="blue darken-1" text @click="addHero">Save</v-btn>
+        <v-btn color="blue darken-1" text @click="validate">Save</v-btn>
       </v-card-actions>
     </v-card>
   </v-dialog>
@@ -51,16 +51,19 @@ export default {
     info: "",
     year: "",
     nameRules: [
+      v => typeof v === "string" || "Must use letters",
       v => (v && v.length <= 30) || "Max 30 characters",
       v => (v && v.length >= 3) || "Min 3 characters"
     ],
     infoRules: [
+      v => typeof v === "string" || "Must use letters",
       v => (v && v.length <= 200) || "Max 200 characters",
       v => (v && v.length >= 3) || "Min 5 characters"
     ],
     yearRules: [
-      v => (v && v.length === 4) || "Valid year",
+      v => !v || v.length == 4 || "Valid year",
       v =>
+        !v ||
         (parseInt(v) && parseInt(v) <= new Date().getUTCFullYear()) ||
         "Valid year"
     ]
@@ -74,10 +77,12 @@ export default {
       data["info"] = this.info;
       // optional fields
       if (this.year) {
-        data["year"] = this.year;
+        data["year"] = parseInt(this.year);
       }
 
-      // ADD VALIDATION
+      // NOTE: glitch (most likely API Problem)
+      // using numbers in name and info field cause problems
+      // try name = 1111, info = 1111, year = 1111
 
       let PORT = this.PORT ? this.PORT : 3000;
       fetch(`http://localhost:${PORT}/api/heroes/`, {
@@ -95,6 +100,12 @@ export default {
           this.heroes.push(data);
         })
         .catch(err => console.log(err));
+    },
+    validate() {
+      if (this.$refs.form.validate()) {
+        this.addHero();
+        this.$refs.form.reset();
+      }
     }
   }
 };
